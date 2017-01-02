cce96cf100f39a0d4fb4cb1c357af95bfda87738
Kamran Ayub <kamran.ayub@gmail.com>
2017-01-01 23:35:24 -0600

    Update to latest Xamarin.Forms

--- Game/Game.Droid/Resources/Resource.Designer.cs
+++ Game/Game.Droid/Resources/Resource.Designer.cs
@@ -26,7 +26,6 @@ namespace Game.Droid
 		
 		public static void UpdateIdValues()
 		{
-			global::Xamarin.Forms.Platform.Android.Resource.Attribute.actionBarSize = global::Game.Droid.Resource.Attribute.actionBarSize;
 		}
 		
 		public partial class Animation
